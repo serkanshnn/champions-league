a5d939463b58ef051da8216f2a3e33fc8fa8e574
Serkan Şahin <serkan.shnn@gmail.com>
2023-12-08 00:24:55 +0300

    update vite config

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,10 @@ import path from 'path';
 export default defineConfig({
     plugins: [
         laravel({
-            input: 'resources/js/app.js',
+            input: [
+                'resources/css/app.css',
+                'resources/js/app.js',
+            ],
             refresh: true,
         }),
         vue({
